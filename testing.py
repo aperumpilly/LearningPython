041b264181bf7159c30e7597696eb8b3555a572f
aperumpilly <aperumpilly@gmail.com>
2022-04-06 18:53:07 +0530

    Using Break and Continue statements

--- testing.py
+++ testing.py
@@ -7,11 +7,15 @@ noFruit = input("From which fruit you do not want : \n")
 print("Fruits you kept in basket are :")
 for myFruit in fruit:    
     if myFruit == noFruit :
-     break
+     break    #Break the FOR loop and
+    elif myFruit[0] == 'A':   #To skip Apple 
+        continue    #continue the FOR loop
     print(myFruit)
 else:
     print("All done")
 print("The Fruit which are skipped \n")
 for leftFruit in range(fruit.index(noFruit) ,len(fruit)):        
         print(fruit[leftFruit])
+
+
 
