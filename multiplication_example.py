7260f5347abc074a81071f0579623f2650ba62ec
aperumpilly <aperumpilly@gmail.com>
2022-04-06 20:50:26 +0530

    Example of Multipication, Greeting of person name starting with D

---
+++ multiplication_example.py
@@ -0,0 +1,18 @@
+""" number = int(input("Enter the number to get multiplication table \n"))
+for i  in range(1,11):
+    
+    print( number , " X " , str(i).zfill(2) , " = " , str(i*number).zfill(2)) """
+
+
+# Program to greet all person name starting with D
+
+nm = ["Ashish", "Dona", "Murli", "Danny", "Dulquer","Syam", "Amrit"]
+for i in nm:
+  if  i[0] =="D":
+      print("Welcome ",i)
+  
+
+
+
+
+
