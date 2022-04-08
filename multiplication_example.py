ca3b1670193c3da486c74e1aa5ebc5cc824c1e87
aperumpilly <aperumpilly@gmail.com>
2022-04-08 18:15:33 +0530

    Program added to multiply using while loop

--- multiplication_example.py
+++ multiplication_example.py
@@ -1,15 +1,17 @@
-""" number = int(input("Enter the number to get multiplication table \n"))
-for i  in range(1,11):
-    
-    print( number , " X " , str(i).zfill(2) , " = " , str(i*number).zfill(2)) """
-
 
 # Program to greet all person name starting with D
 
 nm = ["Ashish", "Dona", "Murli", "Danny", "Dulquer","Syam", "Amrit"]
-for i in nm:
+""" for i in nm:
   if  i[0] =="D":
-      print("Welcome ",i)
+      print("Welcome ",i) """
+
+# Program to get number from user and then show table
+i=1
+numb = int(input("Enter the number for multiplication : \n"))
+while i<11 :        
+        print(f"{i}   X  {numb} =" , i*numb )
+        i +=1
   
 
 
