34ab81a29b8e1025c237d92cee00678c5fc14563
aperumpilly <aperumpilly@gmail.com>
2022-04-08 18:39:15 +0530

    Program to check the prime number

--- multiplication_example.py
+++ multiplication_example.py
@@ -1,5 +1,5 @@
 
-# Program to greet all person name starting with D
+'''# Program to greet all person name starting with D
 
 nm = ["Ashish", "Dona", "Murli", "Danny", "Dulquer","Syam", "Amrit"]
 """ for i in nm:
@@ -12,6 +12,21 @@ numb = int(input("Enter the number for multiplication : \n"))
 while i<11 :        
         print(f"{i}   X  {numb} =" , i*numb )
         i +=1
+''' 
+# Program to check the prime number
+
+i = int(input(" Enter the number : "))
+isPrime = True
+for prime in range(2,i):
+    if(i%prime ==0):
+        isPrime = False
+        break
+if isPrime == True:
+    print( f"{i} is a prime number")
+else:
+    print(f"{i} is nota prime number")
+
+
   
 
 
