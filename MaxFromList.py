8badebab3ae8eeed890a06711372cd9148e58cda
aperumpilly <aperumpilly@gmail.com>
2022-04-09 16:18:19 +0530

    Get the max number as Celsius and convert to FarFarhenheit

--- MaxFromList.py
+++ MaxFromList.py
@@ -4,4 +4,8 @@ a =[]
 for i in range(num):
     intValue= int(input(f"Enter the {i+1} number  : "))
     a.append(intValue)
-print(f"Out of {a} maximum is :" ,max(a))    
+maxValue = max(a)
+print(f"Out of {a} maximum is :" ,maxValue)    
+
+print(f"The Fahrenheit conversion of {maxValue}°C is : ", (maxValue * 9/5) + 32)
+#(0°C × 9/5) + 32 = 32°F
