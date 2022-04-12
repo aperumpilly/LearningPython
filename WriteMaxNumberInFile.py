8a4fbfed317af1eec2aee843c07c30019c54d6e5
aperumpilly <aperumpilly@gmail.com>
2022-04-12 20:02:47 +0530

    Updated with program description

--- WriteMaxNumberInFile.py
+++ WriteMaxNumberInFile.py
@@ -1,3 +1,10 @@
+'''
+This program is to take number from user and check if it is
+greater than the number in the file F:\MyNumberFile.txt 
+and update the file with it.
+If the user number is smaller then do not update
+'''
+
 newNumber = int(input("Enter the new Number : \n"))  #Get the new number frol console
 with open("F:\MyNumberFile.txt") as f:   #Mode by default is Read
     existNumber = f.read()               # Read the file content
